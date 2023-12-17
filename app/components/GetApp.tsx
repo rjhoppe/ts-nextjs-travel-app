e9cccca7dc6904f3a6b899e5ea1e22582c5d430a
rjhoppe-W <48058894+rjhoppe@users.noreply.github.com>
2023-12-17 14:00:14 -0500

    feat: Finished navbar section and began fleshing out init component builds

---
+++ app/components/GetApp.tsx
@@ -0,0 +1,6 @@
+const GetApp = () => {
+  return(
+    <div>GetApp</div>
+  )
+}
+export default GetApp
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
+import Camp from "@/app/components/Camp"
+import Features from "@/app/components/Features";
+import GetApp from "@/app/components/GetApp";
+import Guide from "@/app/components/Guide";
+import Hero from "@/app/components/Hero";
+
 export default function Home() {
   return (
-    <h1 className='font-bold text-teal-500 text-2xl'>
-      Hello World
-    </h1>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
   )
 }
